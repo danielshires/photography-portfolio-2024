5ebd69f0ed72a12054ef7e45ab3208721b22f44a
danielshires <hello@danielshires.com>
2024-04-26 13:36:57 +1000

    Completed collections tutorial

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 import { z, defineCollection } from 'astro:content';
 
-const postCollection = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		date: z.string(),
-		slug: z.string(),
-		tags: z.array(z.string()),
-		content: z.string(),
-		image: z.object({
-			url: z.string(),
-			alt: z.string(),
-		}),
-	}),
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
 });
 
 export const collections = {
-	posts: postCollection,
+	posts: postsCollection,
 };
